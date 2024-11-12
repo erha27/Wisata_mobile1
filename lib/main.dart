98f0b5da6b0b487fd6ed1efd56a19ba5a6b8e802
Robby Dwi Herdiyana <robbyherdiyana7@gmail.com>
2024-11-12 14:45:13 +0700

    membuat landing page

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,12 @@
+import 'package:aslab_travel/latihan_modul/pertemuan5.dart';
+import 'package:aslab_travel/latihan_modul/pertemuan6.dart';
 import 'package:aslab_travel/screens/SplashScreen.dart';
+import 'package:aslab_travel/screens/landing_page.dart';
 import 'package:flutter/material.dart';
 
-void main(){
+void main() {
   runApp(TravelApp());
 }
-
-
 
 class TravelApp extends StatelessWidget {
   const TravelApp({super.key});
@@ -14,7 +15,7 @@ class TravelApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home : Splashscreen(),
+      home: LandingPageOne(),
     );
   }
 }
